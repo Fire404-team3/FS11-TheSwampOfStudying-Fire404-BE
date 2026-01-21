9de03b3383e9147f4269d726df474b7f8a5a9169
eungyeolbaek <eungyeolbaek@gmail.com>
2026-01-21 16:39:04 +0900

    "feat: Prisma 스키마 수정, 상수/예외/미들웨어 초기 세팅"

---
+++ src/exceptions/not-found.exception.js
@@ -0,0 +1,8 @@
+import { HttpExeption } from './http.exception.js';
+import { ERROR_MESSAGE } from '#constants';
+
+export class NotFoundException extends HttpExeption {
+  constructor(message = ERROR_MESSAGE.RESOURCE_NOT_FOUND, details = null) {
+    super(404, message, details);
+  }
+}
