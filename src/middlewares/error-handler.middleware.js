0d90baa067eee8c1d00383485a335fc1330dcafd
eungyeolbaek <eungyeolbaek@gmail.com>
2026-01-21 17:19:47 +0900

    Feat: 에러 핸들러 및 CORS 미들웨어 서버 연결

--- src/middlewares/error-handler.middleware.js
+++ src/middlewares/error-handler.middleware.js
@@ -1,4 +1,4 @@
-import { Prisma } from '#generated/prisma/client.js';
+import { Prisma } from '#generated/prisma/client.ts';
 import { ERROR_MESSAGE, HTTP_STATUS, PRISMA_ERROR } from '#constants';
 import { HttpExeption } from '#exceptions';
 
