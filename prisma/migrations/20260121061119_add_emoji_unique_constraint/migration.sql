b93dcc7ffcf760680b99c3d7cf4a2ea27ba17dc0
eungyeolbaek <eungyeolbaek@gmail.com>
2026-02-03 18:49:25 +0900

    fix: 프리즈마 스키마 해빗 테이블에 onDelete 제약조건 추가

--- prisma/migrations/20260121061119_add_emoji_unique_constraint/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[studyId,emojiType]` on the table `EmojiLog` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "EmojiLog_studyId_emojiType_key" ON "EmojiLog"("studyId", "emojiType");
--- prisma/migrations/20260203093931_init/migration.sql
+++ prisma/migrations/20260203093931_init/migration.sql
@@ -1,23 +1,14 @@
 -- CreateTable
-CREATE TABLE "User" (
-    "id" TEXT NOT NULL,
-    "nickname" TEXT NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-
-    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
-);
-
--- CreateTable
 CREATE TABLE "Study" (
     "id" TEXT NOT NULL,
+    "nickname" TEXT NOT NULL,
     "name" TEXT NOT NULL,
     "description" TEXT NOT NULL,
     "background" TEXT NOT NULL,
     "password" TEXT NOT NULL,
-    "points" INTEGER NOT NULL,
+    "points" INTEGER NOT NULL DEFAULT 0,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
-    "userId" TEXT NOT NULL,
 
     CONSTRAINT "Study_pkey" PRIMARY KEY ("id")
 );
@@ -26,9 +17,9 @@ CREATE TABLE "Study" (
 CREATE TABLE "Habit" (
     "id" TEXT NOT NULL,
     "studyId" TEXT NOT NULL,
-    "habitName" TEXT NOT NULL,
-    "startDate" TIMESTAMP(3) NOT NULL,
-    "endDate" TIMESTAMP(3),
+    "name" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "isDeleted" BOOLEAN NOT NULL DEFAULT false,
 
     CONSTRAINT "Habit_pkey" PRIMARY KEY ("id")
 );
@@ -38,7 +29,6 @@ CREATE TABLE "HabitRecord" (
     "id" TEXT NOT NULL,
     "habitId" TEXT NOT NULL,
     "checkDate" TIMESTAMP(3) NOT NULL,
-    "isChecked" BOOLEAN NOT NULL DEFAULT false,
 
     CONSTRAINT "HabitRecord_pkey" PRIMARY KEY ("id")
 );
@@ -53,11 +43,11 @@ CREATE TABLE "EmojiLog" (
     CONSTRAINT "EmojiLog_pkey" PRIMARY KEY ("id")
 );
 
--- AddForeignKey
-ALTER TABLE "Study" ADD CONSTRAINT "Study_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+-- CreateIndex
+CREATE UNIQUE INDEX "EmojiLog_studyId_emojiType_key" ON "EmojiLog"("studyId", "emojiType");
 
 -- AddForeignKey
-ALTER TABLE "Habit" ADD CONSTRAINT "Habit_studyId_fkey" FOREIGN KEY ("studyId") REFERENCES "Study"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "Habit" ADD CONSTRAINT "Habit_studyId_fkey" FOREIGN KEY ("studyId") REFERENCES "Study"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "HabitRecord" ADD CONSTRAINT "HabitRecord_habitId_fkey" FOREIGN KEY ("habitId") REFERENCES "Habit"("id") ON DELETE CASCADE ON UPDATE CASCADE;
