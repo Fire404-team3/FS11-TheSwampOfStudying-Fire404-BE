e52c4c01e26bb2d0237d90aae78dbbf3f43ea108
eungyeolbaek <eungyeolbaek@gmail.com>
2026-02-06 01:35:44 +0900

    fix: cors에 PATCH 메소드 추가

--- src/middlewares/cors.middleware.js
+++ src/middlewares/cors.middleware.js
@@ -3,7 +3,7 @@ import cors from 'cors';
 const whitelist = ['https://theswampofstudying.netlify.app'];
 
 const corsOptions = {
-  origin: function(origin, callback) {
+  origin: function (origin, callback) {
     if (!origin || whitelist.includes(origin)) {
       callback(null, true);
     } else {
@@ -11,7 +11,7 @@ const corsOptions = {
     }
   },
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
 };
 
 export default cors(corsOptions);
